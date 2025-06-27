841352fbb2b252c86bd27fa3a50c13e5287655d5
davey <davey.rpt@gmail.com>
2025-06-27 10:41:53 +0800

    test api

--- app/Http/Controllers/TelegramWebhookController.php
+++ app/Http/Controllers/TelegramWebhookController.php
@@ -9,23 +9,43 @@ class TelegramWebhookController extends Controller
 {
     public function handle(Request $request)
     {
-        $data = $request->all();
-        Log::info('Telegram webhook:', $data);
+        // $data = $request->all();
+        // Log::info('Telegram webhook:', $data);
 
-        // Example: get text message
+        // // Example: get text message
+        // $message = $data['message'] ?? null;
+
+        // if ($message) {
+        //     $text = $message['text'] ?? '';
+        //     $chatId = $message['chat']['id'];
+        //     $location = $message['location'] ?? null;
+
+        //     // Save to incidents table or log
+        //     Log::info("User sent: $text");
+
+        //     if ($location) {
+        //         Log::info("Location: {$location['latitude']}, {$location['longitude']}");
+        //     }
+        // }
+
+        // return response()->json(['ok' => true]);
+
+        $data = $request->all();
         $message = $data['message'] ?? null;
 
         if ($message) {
-            $text = $message['text'] ?? '';
+            $text = $message['text'] ?? null;
             $chatId = $message['chat']['id'];
             $location = $message['location'] ?? null;
 
-            // Save to incidents table or log
-            Log::info("User sent: $text");
-
-            if ($location) {
-                Log::info("Location: {$location['latitude']}, {$location['longitude']}");
-            }
+            $incident = new Incident();
+            $incident->source = 'telegram';
+            $incident->message = $text;
+            $incident->chat_id = $chatId;
+            $incident->latitude = $location['latitude'] ?? null;
+            $incident->longitude = $location['longitude'] ?? null;
+            $incident->status = 'baru';
+            $incident->save();
         }
 
         return response()->json(['ok' => true]);
